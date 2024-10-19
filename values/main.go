e8b1b0f189fb41ab8ccc75b7cfb7b82a1a2cfe32
Willian Silva <silva.williansantos@gmail.com>
2024-10-19 12:14:14 -0300

    add values and variables examples

---
+++ values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("GO" + "lang")
+	fmt.Println("I'm software developer")
+
+	fmt.Println("44 + 55 = ", 44+55)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
