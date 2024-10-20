bd8c3e43551ae1590ff2f926be406504c9d2311d
Willian Silva <silva.williansantos@gmail.com>
2024-10-20 09:51:43 -0300

    add switch example

--- switch/main.go
+++ switch/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
 
+func main() {
+	os := runtime.GOOS
+	switch os {
+	case "linux":
+		fmt.Println("Linux")
+	case "darwin":
+		fmt.Println("macOS")
+	default:
+		fmt.Println(os)
+	}
+
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today")
+	case today + 1:
+		fmt.Println("Tomorrow")
+	case today + 2:
+		fmt.Println("In two days")
+	default:
+		fmt.Println("To far way")
+	}
+
+	time := time.Now()
+	switch {
+	case time.Hour() < 12:
+		fmt.Println("Good Morning")
+	case time.Hour() < 17:
+		fmt.Println("Good Afternoon")
+	default:
+		fmt.Println("Good Evening")
+	}
 }
